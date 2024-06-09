ad2e079c7ecd3f53e5cf433a16dc78e1e707c01b
Alexandre <alexandre.caissy@norda.com>
2024-06-09 12:11:15 -0400

    feat: added meals detail page

---
+++ app/meals/[mealId]/page.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import { getMeal } from "@/lib/meals";
+
+const MealIDPage = async ({ params }) => {
+  const meal = await getMeal(params.mealId);
+
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image fill src={meal.image} alt={meal.title} />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            By: <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main className={styles.main}>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions.replace(/\n/g, "<br />"),
+          }}
+        ></p>
+      </main>
+    </>
+  );
+};
+
+export default MealIDPage;
---
+++ app/not-found.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <main className="not-found">
+      <h1>The page you are longer for doesn't exist..</h1>
+    </main>
+  );
+};
+
+export default NotFound;
