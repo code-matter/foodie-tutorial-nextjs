be3ea86426394726f3f801d49fbd31540bf0156f
Alexandre <alexandre.caissy@norda.com>
2024-06-09 20:13:39 -0400

    removing a meal

--- components/meals/MealItem.jsx
+++ components/meals/MealItem.jsx
@@ -1,9 +1,16 @@
+'use client'
+
 import Link from 'next/link'
 import Image from 'next/image'
 
 import styles from './meal-item.module.css'
+import { deleteMeal } from '@/lib/actions'
 
 export default function MealItem({ title, mealId, image, summary, creator }) {
+    const handleDeleteMeal = async () => {
+        await deleteMeal(mealId)
+    }
+
     return (
         <article className={styles.meal}>
             <header>
@@ -19,6 +26,7 @@ export default function MealItem({ title, mealId, image, summary, creator }) {
                 <p className={styles.summary}>{summary}</p>
                 <div className={styles.actions}>
                     <Link href={`/meals/${mealId}`}>View Details</Link>
+                    <p onClick={handleDeleteMeal}>Remove</p>
                 </div>
             </div>
         </article>
